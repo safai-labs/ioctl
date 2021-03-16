227f25f9d62d9ef0136b7b7439770558fb9c82b7
Cody P Schafer <dev@codyps.com>
2021-03-15 21:51:43 -0400

    cargo fmt

--- ioctl-sys/src/lib.rs
+++ ioctl-sys/src/lib.rs
@@ -27,11 +27,11 @@ use platform_not_supported;
 #[cfg(doctest)]
 mod test_readme {
     macro_rules! external_doc_test {
-    ($x:expr) => {
-        #[doc = $x]
-        extern {}
-    };
-  }
+        ($x:expr) => {
+            #[doc = $x]
+            extern "C" {}
+        };
+    }
 
     external_doc_test!(include_str!("../../README.md"));
 }
